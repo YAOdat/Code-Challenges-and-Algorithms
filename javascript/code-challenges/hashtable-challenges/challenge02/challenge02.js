3e253bb86d092e299dd4f8f6f5b3e06a45b49e6d
Yaser Odat <yaserodat@outlook.com>
2022-11-09 20:50:12 +0000

    solved challenge 02

--- javascript/code-challenges/hashtable-challenges/challenge02/challenge02.js
+++ javascript/code-challenges/hashtable-challenges/challenge02/challenge02.js
@@ -1,1 +1,18 @@
-// Write here the code challenge solution
+'use strict';
+
+function firstRepeatedWord(str) {
+    const words = str.split(' ');
+    const hash = {};
+    for (let i = 0; i < words.length; i++) {
+
+        if (hash[words[i]]) {
+          
+        return words[i];
+        } else {
+        hash[words[i]] = true;
+        }
+    }
+    return 'No repeated words';
+    }
+
+module.exports = firstRepeatedWord;
